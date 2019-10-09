293421f02ce00a3aa3314a2ed377931cc2438fad
pierz barry <pmbarry@asu.edu>
2019-10-08 21:14:28 -0400

    first commit

---
+++ src/main/java/com/example/demo/Exercise503Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise503Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise503Application.class, args);
+    }
+
+}
